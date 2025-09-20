3c679ef342e41236451bc9ef0bbbf3d2b8e6186a
David <david.juracsikk@gmail.com>
2025-09-20 11:10:18 +0200

    Update PortfolioOptimiser.jl to use PyCall

--- src/PortfolioOptimiser.jl
+++ src/PortfolioOptimiser.jl
@@ -5,7 +5,7 @@
 module PortfolioOptimiser
 using AverageShiftedHistograms, Clustering, DataFrames, Dates, Distances, Distributions,
       GLM, JuMP, LinearAlgebra, MultivariateStats, NearestCorrelationMatrix, Optim, Graphs,
-      SimpleWeightedGraphs, PythonCall, Random, SmartAsserts, SparseArrays, Statistics,
+      SimpleWeightedGraphs, PyCall, Random, SmartAsserts, SparseArrays, Statistics,
       StatsBase, TimeSeries
 
 # Turn readme into PortfolioOptimiser's docs.
